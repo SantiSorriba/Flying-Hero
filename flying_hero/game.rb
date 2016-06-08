4d1ab695ad698169425384bb26952d53990018b2
Santiago Sorriba <santiagosorriba98@gmail.com>
2016-06-08 19:53:02 -0300

    Juego Flying Hero Ruby 08/06/2016

--- flying_hero/game.rb
+++ flying_hero/game.rb
@@ -1,13 +1,20 @@
 require 'gosu'
+require_relative 'background'
 
 class Game < Gosu::Window
 
   def initialize
     super(900, 550, fullscreen: false)
     self.caption = 'Flying Hero!'
+    @background = Background.new
   end
 
   def draw
+    @background.draw
+  end
+
+  def update
+    @background.scroll!
   end
 
   def button_down(id)
